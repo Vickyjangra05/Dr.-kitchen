cccae04fd70ed0ad4b383d5b872854efac30c01b
Vikram Singh <Vickyjangra7891@gmail.com>
2025-10-31 11:15:43 +0530

    ngrok

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss() ],
+  server: {
+    allowedHosts: [
+      'nonfungible-crinal-veola.ngrok-free.dev'
+    ],
+    proxy: {
+      // This will proxy any request starting with /api to your backend server
+      '/api': {
+        target: 'http://localhost:8000', // Change this if your backend runs on a different port
+        changeOrigin: true, // Recommended for avoiding CORS issues
+        secure: false,      // Set to false for local development with http
+      },
+    },
+  },
 })
